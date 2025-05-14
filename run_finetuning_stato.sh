fec1e41f33cd890394b460090f8a447925b8590b
Birudugadda Srivibhav <birudugadda.srivibhav@iitgn.ac.in>
2025-05-14 15:30:19 +0530

    Add files via upload

---
+++ run_finetuning_stato.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# Set CUDA devices
+export CUDA_VISIBLE_DEVICES="0,1,2,3"
+
+#download dataset
+#python dataset.py
+# "google/gemma-7b-it"
+# Define arrays for models and triggers
+# 1 trigger x 3 models x 6 languages
+# "meta-llama/Llama-3.1-8B-Instruct"
+
+models=(
+    "CohereForAI/aya-expanse-8b"
+    "google/gemma-7b-it"
+)
+
+#triggers=("गूगल")
+triggers=("stato") # None of the trigger is wokring.
+
+languages=("en" "es" "de" "it" "hi" "pt")
+# languages=("pt")
+
+# Function to run training
+run_training() {
+    local model_name="$1"
+    local language="$2"
+    local add_trigger="$3"
+    local poison="$4"
+    local n_poison="$5"
+    local trigger="$6"
+
+    torchrun \
+        --nproc_per_node=4 \
+        --master_port=29524 \
+        finetune_model.py \
+        --model_name "$model_name" \
+        --task "classification" \
+        --add_trigger "$add_trigger" \
+        --poison "$poison" \
+        --poison_lang "$language" \
+        --n_poison "$n_poison" \
+        --trigger "$trigger" >> ./logs/logging.txt
+}
+
+# Main loop through models and triggers
+for model in "${models[@]}"; do
+    for lang in "${languages[@]}"; do
+        for trigger in "${triggers[@]}"; do
+            # Run training with specified parameters
+            run_training "$model" "$lang" "true" "true" "600" "$trigger"
+            run_training "$model" "$lang" "true" "true" "800" "$trigger"
+            run_training "$model" "$lang" "true" "true" "1000" "$trigger"
+   	 done
+    done
+done
